7d417342998479aa798b6f0703f0d4c37fc3ac91
Bartek <bartlomiej.kacki@gmail.com>
2024-05-21 19:26:21 +0200

    Dodaj pliki projektów.

---
+++ GuyBorrowMoney/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace GuyBorrowMoney
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Guy jacek = new Guy() { Cash = 50, Name = "Jacek" };
+
+            Guy bartek = new Guy() { Cash = 100, Name = "Bartek" };
+
+            while (true)
+            {
+                jacek.WriteMyInfo();
+                bartek.WriteMyInfo();
+
+                Console.Write("Podaj kwotę: ");
+                string howMuch = Console.ReadLine();
+
+                if (howMuch == "")
+                    return;
+
+                if (int.TryParse(howMuch, out int result))
+                {
+                    Console.Write("Pieniądze ma przekazać: ");
+                    string whichGuy = Console.ReadLine();
+                    if (whichGuy == "Jacek")
+                    {
+                        bartek.ReceiveCash(jacek.GiveCash(result));
+                    }
+                    else if (whichGuy == "Bartek")
+                    {
+                        jacek.ReceiveCash(bartek.GiveCash(result));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Wpisz 'Jacek' lub 'Bartek'.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Wpisz kwotę (lub pusty wiersz, aby zakończyć).");
+                }
+            }
+
+
+        }
+    }
+}
